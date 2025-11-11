b5bb2e503de1be2382a8bc8c2e708ca1184b5ad7
hammambys <hammambenyounes4@gmail.com>
2025-11-11 15:25:16 +0100

    change project name

--- data.js
+++ data.js
@@ -11,10 +11,10 @@ export const about = {
 
 export const portfolio = [
   {
-    title: "Baby name generator - AI project",
-    summary:"AI-powered web app built with FastAPI and LangChain that suggests unique and meaningful names depending on user preferences",
-    description:
-      `This is a personal project that leverages the power of AI to help parents find the perfect name for their baby.
+    title: "AI Baby name generator",
+    summary:
+      "AI-powered web app built with FastAPI and LangChain that suggests unique and meaningful names depending on user preferences",
+    description: `This is a personal project that leverages the power of AI to help parents find the perfect name for their baby.
       The application uses FastAPI for the backend and LangChain to interact with LLMs, providing users with unique and meaningful name suggestions based on their preferences.`,
     features: [
       "AI-powered name suggestions",
@@ -23,17 +23,17 @@ export const portfolio = [
     ],
     tools: ["FastAPI", "Python", "LangChain", "Groq API"],
     source: "https://github.com/hammambys/human-name-generator",
-    link:"https://human-name-generator.onrender.com",
+    link: "https://human-name-generator.onrender.com",
     slug: "baby-name-generator",
-    screenshots:["/images/baby_name_2.png"],
-    img:"/images/baby_name_2.png"
+    screenshots: ["/images/baby_name_2.png"],
+    img: "/images/baby_name_2.png",
   },
   {
     title: "AI Business Advisor Council",
-    summary:"AI-powered web app that evaluates business ideas and expert feedback using Groq API",
-    img:"/images/advisor_1.png",
-    description:
-      `This is a personal project that leverages the power of AI to help entrepreneurs evaluate their business ideas.
+    summary:
+      "AI-powered web app that evaluates business ideas and expert feedback using Groq API",
+    img: "/images/advisor_1.png",
+    description: `This is a personal project that leverages the power of AI to help entrepreneurs evaluate their business ideas.
       It is like having a advisor council composed of Strategist, Technical expert and Legal advisor to provide feedback on your business idea.
       The application uses LangChain to interact with LLMs via Groq API, providing users with comprehensive evaluations and expert advice.`,
     features: [
@@ -42,16 +42,16 @@ export const portfolio = [
       "Comprehensive analysis and advice",
     ],
     tools: ["Streamlit", "Python", "LangChain", "Groq API"],
-    source:"https://github.com/hammambys/business-mentoring-council",
+    source: "https://github.com/hammambys/business-mentoring-council",
     link: "https://business-advisor-council.streamlit.app",
     slug: "business-advisor-council",
-    screenshots:["/images/advisor_1.png"],
+    screenshots: ["/images/advisor_1.png"],
   },
   {
     title: "Manageme - Full-stack e-learning platform",
-    summary:"An e-learning project management courses platform built with Angular and Spring Boot that enables interactive course management ",
-    description:
-      `This was an academic project at the end of second year in college which is an e-learning platform designed to manage training programs and educational courses related to project management. 
+    summary:
+      "An e-learning project management courses platform built with Angular and Spring Boot that enables interactive course management ",
+    description: `This was an academic project at the end of second year in college which is an e-learning platform designed to manage training programs and educational courses related to project management. 
       This platform supports multiple content types, allowing educators and trainers to create a rich, interactive learning experience.
       It enables users to generate their own content, providing a collaborative learning environment.`,
     features: [
@@ -64,29 +64,28 @@ export const portfolio = [
     tools: ["Angular", "Spring boot", "MySQL"],
     source: "https://github.com/hammambys/manageme",
     slug: "manageme",
-    screenshots:["/images/manageme_1.jpg","/images/manageme_2.jpg"],
-    img:"/images/manageme_1.jpg"
+    screenshots: ["/images/manageme_1.jpg", "/images/manageme_2.jpg"],
+    img: "/images/manageme_1.jpg",
   },
   {
     title: "Association Robotique ENSI - Landing page",
-    summary:"Landing page for the official website of Association Robotique ENSI",
+    summary:
+      "Landing page for the official website of Association Robotique ENSI",
     img: "/images/are.PNG",
-    description:
-      `This is my first real-world coding project while I was in college which is the design and development of the official website for the Association Robotique ENSI. 
+    description: `This is my first real-world coding project while I was in college which is the design and development of the official website for the Association Robotique ENSI. 
       The landing page features a modern design, reflecting the innovative spirit of the association.`,
-    features: ["Landing page", "Contact form","Photo gallery"],
-    tools: ["React", "Figma","Bootstrap"],
+    features: ["Landing page", "Contact form", "Photo gallery"],
+    tools: ["React", "Figma", "Bootstrap"],
     link: "http://www.ar-ensi.tn/",
     slug: "landing-page-are",
-    screenshots:["/images/are.png"]
-
+    screenshots: ["/images/are.png"],
   },
   {
     title: "Mumia - Full-stack e-commerce web application",
-    summary:"A jumia.com clone built with React and Express.js that enables online buying and selling of products",
+    summary:
+      "A jumia.com clone built with React and Express.js that enables online buying and selling of products",
     img: "/images/mumia.PNG",
-    description:
-      `This is a fun project i did which consists of cloning jumia.com, a popular e-commerce platform in my region. 
+    description: `This is a fun project i did which consists of cloning jumia.com, a popular e-commerce platform in my region. 
       This project is fully-featured marketplace website designed from scratch to facilitate online buying and selling. 
       It includes a range of features to enhance the user experience, such as a streamlined signup/signin process, vendor dashboards, and integrated payment systems. 
       It is designed to be mobile-friendly, ensuring accessibility across all devices.`,
@@ -102,75 +101,75 @@ export const portfolio = [
     tools: ["MongoDB", "React", "Redux", "Express.js", "Node.js"],
     source: "https://github.com/hammambys/mumia-app",
     slug: "mumia",
-    screenshots:["/images/mumia.png"]
+    screenshots: ["/images/mumia.png"],
   },
   {
     title: "Cabinet Rabii Absi - Consulting firm website",
-    summary:"A website for the consulting firm 'Cabinet Rabii Absi' built with Node.js and Express.js",
+    summary:
+      "A website for the consulting firm 'Cabinet Rabii Absi' built with Node.js and Express.js",
     img: "/images/consulting_2.jpg",
-    description:
-      `This is a freelance project which consists of developing a website for the consulting firm 'Cabinet Rabii Absi'
+    description: `This is a freelance project which consists of developing a website for the consulting firm 'Cabinet Rabii Absi'
       The website includes a landing page, a blog, appointement scheduling system, an admin dashboard to manage clients and blog posts ...`,
     features: [
       "Landing page",
       "Blog",
       "Appointment scheduling",
       "Admin dashboard",
-      "Mobile Friendliness"
+      "Mobile Friendliness",
     ],
-    tools: ["Express.js", "Node.js","Handlebars"],
+    tools: ["Express.js", "Node.js", "Handlebars"],
     source: "https://github.com/hammambys/site-consulting",
     slug: "consulting",
-    screenshots:["/images/consulting_1.jpg","/images/consulting_2.jpg"]
+    screenshots: ["/images/consulting_1.jpg", "/images/consulting_2.jpg"],
   },
   {
     title: "PowerZone - Landing page",
-    summary:"Landing page for local gym PowerZone built with HTML,CSS and Bootstrap",
+    summary:
+      "Landing page for local gym PowerZone built with HTML,CSS and Bootstrap",
     img: "/images/powerzone.PNG",
-    description:
-      `This is a freelance project which consists of developing a landing page for gym PowerZone
+    description: `This is a freelance project which consists of developing a landing page for gym PowerZone
       The landing page features a modern design, reflecting the innovative spirit of the gym.`,
-    features: ["Landing page", "Contact form","Responsive design"],
-    tools: ["HTML", "CSS","Bootstrap"],
-    source:"https://github.com/hammambys/power-zone-website",
+    features: ["Landing page", "Contact form", "Responsive design"],
+    tools: ["HTML", "CSS", "Bootstrap"],
+    source: "https://github.com/hammambys/power-zone-website",
     slug: "landing-page-powerzone",
-    screenshots:["/images/powerzone.png"]
-  }
-  
+    screenshots: ["/images/powerzone.png"],
+  },
 ];
 
 export const experience = [
   {
     date: "2023 — Present",
     title: "Software Engineer",
-    link:"https://www.fisglobal.com/",
+    link: "https://www.fisglobal.com/",
     company: "FIS",
-    description: "Worked with the UBIX Bespoke team to develop, maintain and deliver custom enhancements in UBIX software for 10+ clients in financial sector around the world. Clients include BBVA, BNP, Pictet and more.",
+    description:
+      "Worked with the UBIX Bespoke team to develop, maintain and deliver custom enhancements in UBIX software for 10+ clients in financial sector around the world. Clients include BBVA, BNP, Pictet and more.",
     technologies: ["Python", "PL/SQL"],
   },
   {
     date: "Feb 2023 — Jul 2023",
     title: "Full-stack engineer intern",
-    link:"https://gewinner.tn/",
+    link: "https://gewinner.tn/",
     company: "Gewinner",
-    description: "Designed, implemented and shipped a web application for medical device monitoring using MEAN stack",
-    technologies: ["Angular", "MongoDB","Express.js","Node.js"],
+    description:
+      "Designed, implemented and shipped a web application for medical device monitoring using MEAN stack",
+    technologies: ["Angular", "MongoDB", "Express.js", "Node.js"],
   },
   {
     date: "Nov 2022 — Feb 2023",
     title: "Freelance developer",
-    link:"https://gewinner.tn/",
+    link: "https://gewinner.tn/",
     company: "Gewinner",
     description: `Created new features for the official company's website using Node.js, Angular and MongoDB.`,
-    technologies: ["Angular", "MongoDB","Express.js","Node.js"],
+    technologies: ["Angular", "MongoDB", "Express.js", "Node.js"],
   },
   {
     date: "Jun 2022 — Aug 2022",
     title: "QA engineer intern",
-    link:"https://sama-consulting.com/",
+    link: "https://sama-consulting.com/",
     company: "Sama consulting",
     description: `Established a testing and validation process of medical laboratory management software using Cypress, identifying and reporting critical bugs.`,
-    technologies: ["Cypress", "Jenkins","Turtoise SVN"],
+    technologies: ["Cypress", "Jenkins", "Turtoise SVN"],
   },
-
 ];
